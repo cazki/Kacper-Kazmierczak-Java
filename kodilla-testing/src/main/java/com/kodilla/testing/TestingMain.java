de8f9163232f7613e5f293b049cbafa052a93f49
cazki <kapik997@gmail.com>
2019-05-23 15:32:13 +0200

    TestCalculator

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,36 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        Calculator calculator = new Calculator(1, 1);
+        calculator.sum();
+        calculator.subtract();
+        int sumTest = numberA + numberB;
+        int subtractTest = numberA - numberB;
+        if (sumAB == sumTest){
+             System.out.println("Adding is OK");
+        }
+        else{
+             System.out.println("Adding is wrong!!!");
+        }
+        if (subtractAB == subtractTest){
+            System.out.println("Subtract is OK");
+        }
+        else {
+            System.out.println("Subtract is wrong!!!");
+        }
     }
 }
