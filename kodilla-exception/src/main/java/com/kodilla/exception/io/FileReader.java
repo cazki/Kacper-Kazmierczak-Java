8cc99afd8707c347e88e3daf8bb4deb29548fbb7
cazki <kapik997@gmail.com>
2019-06-24 13:16:45 +0200

    8.3

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -9,7 +9,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile(){
+    public void readFile() throws FileReaderException{
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         Path path = Paths.get("files/dd.txt");
@@ -18,7 +18,8 @@ public class FileReader {
             fileLines.forEach(System.out::println);
 
         } catch (IOException e) {
-            System.out.println("Oh no, Something is wrong. Error : " + e);
+            //System.out.println("Oh no, Something is wrong. Error : " + e);
+            throw new FileReaderException();
         }
         finally {
             System.out.println("I am gonna be here ... Always!");
---
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/ExceptionHandling.java
@@ -0,0 +1,18 @@
+package com.kodilla.exception.test;
+
+public class ExceptionHandling {
+
+    public static void main(String[] args) {
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        try {
+            String result = secondChallenge.probablyIWillThrowException(2, 1);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println("Exception: " + e);
+        } finally {
+            System.out.println("I am gonna be here ... Always!");
+        }
+    }
+}
