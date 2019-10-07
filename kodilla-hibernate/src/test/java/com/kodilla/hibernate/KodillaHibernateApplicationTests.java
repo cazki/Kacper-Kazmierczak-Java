ca797d7cb202cb451ad0812ba2687c403f33d9bd
cazki <kapik997@gmail.com>
2019-10-07 19:00:04 +0200

    17.1

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -6,10 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes={KodillaHibernateApplication.class})
 public class KodillaHibernateApplicationTests {
 
     @Test
     public void contextLoads() {
     }
+
 }
